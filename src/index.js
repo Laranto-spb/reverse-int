84c3cdc57d028a62b4d5191311684069312ddc1d
Laranto-spb <laranto1985@gmail.com>
2020-10-23 09:00:30 +0300

    feat: Add reverse solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let moduleNumber = Math.abs(n);
+    let arrNumber = [...moduleNumber.toString()];
+    let reverse = arrNumber.reverse().join('');
+    let result = parseInt(reverse);
+    return result;
 }
